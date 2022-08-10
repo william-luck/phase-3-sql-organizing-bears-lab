3bd4faf94bfce0d53365c21a7590871c9f7511c8
William Luck <luck.william.h@gmail.com>
2022-08-10 14:33:21 -0500

    commit tree aa54210bf53923ec1041bc3e4ef7b00808c96b5d

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+-- INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'black', 'rusty', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 69, 'F', 'polar', 'crusty', 0); 
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 13, 'F', 'grey', 'rusty', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 56, 'M', 'army green', 'PTSD', 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 10, 'M', 'black', 'rusty', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 200, 'M', 'green', 'heart smol', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 10, 'F', 'frosty', 'cold', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, 'M', 'black', 'rusty', 1);
+
